e0926a47e47c8b24e4e7c9032ca1ced27386925a
fpetrochenkov <filpetrochenkov@gmail.com>
2016-10-25 11:33:16 +0300

    Update CarModelPage.java

--- src/main/java/com/qaprosoft/pages/CarModelPage.java
+++ src/main/java/com/qaprosoft/pages/CarModelPage.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.gui.AbstractPage;
 
-public class CarModelPage extends AbstractPage {	
-//	@FindBy(xpath = "/html/body/div[3]/div[1]/main/div/div[3]/div[1]/ul/li[28]/a/span")
+public class CarModelPage extends AbstractPage {
 	@FindBy(xpath = "//div[@class='brands']//a/span")
 	private List<ExtendedWebElement> modelLinks;
 
